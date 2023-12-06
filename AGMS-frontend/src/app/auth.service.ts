65ebb30b2860a121fd78321b93d51bf65f8f5580
IsharaSamaraweera <isharalasanthi@gmail.com>
2023-12-06 12:57:55 +0530

    Merge branch

--- AGMS-frontend/src/app/auth.service.ts
+++ AGMS-frontend/src/app/auth.service.ts
@@ -7,27 +7,22 @@ import { Observable } from 'rxjs';
 })
 
 export class AuthService {
-    // ...
   
     setAuthToken(token: string): void {
-      // Implement logic to store the token (e.g., in localStorage)
       localStorage.setItem('token', token);
     }
   
     getAuthToken(): string | null {
-      // Implement logic to retrieve the stored token
       return localStorage.getItem('token');
     }
   
     isLoggedIn(): boolean {
-      // Implement logic to check if the user is logged in (e.g., check the token's existence and validity)
       const token = this.getAuthToken();
       return token !== null && this.isTokenValid(token);
     }
   
     private isTokenValid(token: string): boolean {
-      // Implement logic to check if the token is valid (e.g., check expiration)
-      // Return true if valid, false otherwise
+      
       return true;
     }
 
